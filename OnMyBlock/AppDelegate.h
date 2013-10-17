527c41626206dafb73ccc45bff8e199e21090515
Tommy Dang <quantumventuress@gmail.com>
2013-10-17 15:38:12 -0700

    Initial Commit

---
+++ OnMyBlock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OnMyBlock
+//
+//  Created by Tommy DANGerous on 10/17/13.
+//  Copyright (c) 2013 OnMyBlock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OnMyBlockTests/OnMyBlockTests.m
@@ -0,0 +1,34 @@
+//
+//  OnMyBlockTests.m
+//  OnMyBlockTests
+//
+//  Created by Tommy DANGerous on 10/17/13.
+//  Copyright (c) 2013 OnMyBlock. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OnMyBlockTests : XCTestCase
+
+@end
+
+@implementation OnMyBlockTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
