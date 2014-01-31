11964d64f61c660e55b23f9a90b605c70d79aa8c
Tommy Dang <quantumventuress@gmail.com>
2014-01-31 10:03:47 -0800

    removed warning in create listing view controller

--- OnMyBlock/Controllers/Create Listing/OMBCreateListingViewController.m
+++ OnMyBlock/Controllers/Create Listing/OMBCreateListingViewController.m
@@ -943,7 +943,6 @@ replacementString: (NSString *) string
         _temporaryResidence dictionary: valuesDictionary];
     conn.completionBlock = ^(NSError *error) {
       if (_temporaryResidence.uid && !error) {
-        #warning Check to see if user is a landlord type
         // Check to see what type of property it is and then
         // change the user's landlord type
         // Only if the user doesn't already have a landlord type
