12aec152f51017211bd626c019b1480cf7e3a09e
Tommy Dang <quantumventuress@gmail.com>
2014-02-14 00:10:05 -0800

    updated a lot of stuff

---
+++ OnMyBlock/Connections/Messages/OMBMessagesLastFetchedWithUserConnection.h
@@ -0,0 +1,13 @@
+//
+//  OMBMessagesLastFetchedWithUserConnection.h
+//  OnMyBlock
+//
+//  Created by Tommy DANGerous on 2/13/14.
+//  Copyright (c) 2014 OnMyBlock. All rights reserved.
+//
+
+#import "OMBConnection.h"
+
+@interface OMBMessagesLastFetchedWithUserConnection : OMBConnection
+
+@end
---
+++ OnMyBlock/Connections/Messages/OMBMessagesLastFetchedWithUserConnection.m
@@ -0,0 +1,13 @@
+//
+//  OMBMessagesLastFetchedWithUserConnection.m
+//  OnMyBlock
+//
+//  Created by Tommy DANGerous on 2/13/14.
+//  Copyright (c) 2014 OnMyBlock. All rights reserved.
+//
+
+#import "OMBMessagesLastFetchedWithUserConnection.h"
+
+@implementation OMBMessagesLastFetchedWithUserConnection
+
+@end
