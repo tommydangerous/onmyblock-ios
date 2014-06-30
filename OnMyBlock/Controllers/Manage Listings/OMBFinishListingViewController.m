a249f7a37e08deb3df2873c73b8ba22de8630e81
Paul <aguilarpgc@gmail.com>
2014-06-30 16:42:09 -0500

    adjusted frames in create and finish listing

--- OnMyBlock/Controllers/Manage Listings/OMBFinishListingViewController.m
+++ OnMyBlock/Controllers/Manage Listings/OMBFinishListingViewController.m
@@ -39,6 +39,8 @@
 #import "UIImage+Color.h"
 #import "UIImage+Resize.h"
 
+float const photoViewImageHeightPercentage = 0.32;
+
 @implementation OMBFinishListingViewController
 
 #pragma mark - Initializer
@@ -144,7 +146,7 @@
   [unlistView addSubview: unlistButton];
 
   CGFloat visibleImageHeight = screen.size.height *
-    PropertyInfoViewImageHeightPercentage; // ... * 0.4
+    photoViewImageHeightPercentage; // ... * 0.32
   CGFloat headerImageHeight = 44.0f + visibleImageHeight + 44.0f;
 
   // Table header view
