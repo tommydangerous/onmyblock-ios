c12cd4ea5223e85dddb03c5c0863b38f9ce0f732
Paul <aguilarpgc@gmail.com>
2014-07-02 19:12:41 -0500

    Image two textfield

---
+++ OnMyBlock/Views/Users/OMBImageTwoTextFieldCell.h
@@ -0,0 +1,13 @@
+//
+//  OMBImageTwoTextFieldCell.h
+//  OnMyBlock
+//
+//  Created by Paul Aguilar on 7/2/14.
+//  Copyright (c) 2014 OnMyBlock. All rights reserved.
+//
+
+#import "OMBTableViewCell.h"
+
+@interface OMBImageTwoTextFieldCell : OMBTableViewCell
+
+@end
---
+++ OnMyBlock/Views/Users/OMBImageTwoTextFieldCell.m
@@ -0,0 +1,31 @@
+//
+//  OMBImageTwoTextFieldCell.m
+//  OnMyBlock
+//
+//  Created by Paul Aguilar on 7/2/14.
+//  Copyright (c) 2014 OnMyBlock. All rights reserved.
+//
+
+#import "OMBImageTwoTextFieldCell.h"
+
+@implementation OMBImageTwoTextFieldCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
