38ec8ca248e9b174a48bf48c108c147639b40905
Paul <aguilarpgc@gmail.com>
2014-08-06 14:51:46 -0500

    search in schools and neighborhood stores

--- OnMyBlock/Stores/OMBSchoolStore.m
+++ OnMyBlock/Stores/OMBSchoolStore.m
@@ -21,55 +21,65 @@
   
   schools = [NSMutableArray array];
   
+  // Grossmont College
   OMBSchool *gross  = [OMBSchool new];
   gross.coordinate  = CLLocationCoordinate2DMake(32.8163, -117.0063);
   gross.displayName = @"Grossmont College";
   [schools addObject:gross];
   
+  // Mesa College
   OMBSchool *mesa  = [OMBSchool new];
   mesa.coordinate  = CLLocationCoordinate2DMake(32.8044, -117.1703);
   mesa.displayName = @"Mesa College";
   mesa.realName    = @"San Diego Mesa College";
   [schools addObject:mesa];
   
+  // MiraCosta College
   OMBSchool *miraCos  = [OMBSchool new];
   miraCos.coordinate  = CLLocationCoordinate2DMake(33.1906, -117.3013);
   miraCos.displayName = @"MiraCosta College";
   [schools addObject:miraCos];
   
+  // Miramar College
   OMBSchool *miraMar  = [OMBSchool new];
   miraMar.coordinate  = CLLocationCoordinate2DMake(32.9071, -117.1208);
   miraMar.displayName = @"Miramar College";
   miraMar.realName    = @"San Diego Miramar College";
   [schools addObject:miraMar];
   
+  // Palomar College
   OMBSchool *palo  = [OMBSchool new];
   palo.coordinate  = CLLocationCoordinate2DMake(33.1494, -117.1848);
   palo.displayName = @"Palomar College";
   [schools addObject:palo];
   
+  // Pt. Loma University
   OMBSchool *loma  = [OMBSchool new];
   loma.coordinate  = CLLocationCoordinate2DMake(32.7169, -117.2507);
   loma.displayName = @"Pt. Loma University";
   loma.realName    = @"Point Loma Nazarene University";
   [schools addObject:loma];
   
+  // San Diego City College
   OMBSchool *sdiegoCC  = [OMBSchool new];
   sdiegoCC.coordinate  = CLLocationCoordinate2DMake(32.7174, -117.1526);
   sdiegoCC.displayName = @"San Diego City College";
   [schools addObject:sdiegoCC];
   
+  // San Diego State University
   OMBSchool *sdiegoSU  = [OMBSchool new];
   sdiegoSU.coordinate  = CLLocationCoordinate2DMake(32.775619, -117.0713348);
   sdiegoSU.displayName = @"San Diego State University";
   [schools addObject:sdiegoSU];
   
+  // UC San Diego
   OMBSchool *ucSDiego  = [OMBSchool new];
   ucSDiego.coordinate  = CLLocationCoordinate2DMake(32.8754964, -117.2383261);
   ucSDiego.displayName = @"UC San Diego";
   ucSDiego.realName    = @"University of California, San Diego";
   [schools addObject:ucSDiego];
   
+  // University of San Diego
   OMBSchool *univSDiego  = [OMBSchool new];
   univSDiego.coordinate  = CLLocationCoordinate2DMake(32.7719839, -117.187294);
   univSDiego.displayName = @"University of San Diego";
@@ -77,12 +87,14 @@
   
   //---------------------------------------------------------------
   
+  // Cal Poly SLO
   OMBSchool *calPoly  = [OMBSchool new];
   calPoly.coordinate  = CLLocationCoordinate2DMake(35.3017, -120.6598);
   calPoly.displayName = @"Cal Poly SLO";
   calPoly.realName    = @"California Polytechnic State University";
   [schools addObject:calPoly];
   
+  // Cuesta College
   OMBSchool *cuesta  = [OMBSchool new];
   cuesta.coordinate  = CLLocationCoordinate2DMake(35.3300, -120.7424);
   cuesta.displayName = @"Cuesta College";
@@ -91,7 +103,7 @@
   //---------------------------------------------------------------
   
   OMBSchool *none   = [OMBSchool new];
-  cuesta.coordinate = CLLocationCoordinate2DMake(0.0, 0.0);
+  none.coordinate   = CLLocationCoordinate2DMake(0.0, 0.0);
   none.displayName  = @"None";
   none.realName     = @"";
   [schools addObject:none];
